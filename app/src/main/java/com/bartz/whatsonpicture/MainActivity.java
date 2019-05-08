21278c39bd860000859d61a1c1887f927b58b53d
BartZ <bartosz93zak@gmail.com>
2019-05-08 02:24:53 +0200

    -chosing picture from gallery

--- app/src/main/java/com/bartz/whatsonpicture/MainActivity.java
+++ app/src/main/java/com/bartz/whatsonpicture/MainActivity.java
@@ -1,17 +1,24 @@
 package com.bartz.whatsonpicture;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -34,7 +41,7 @@ import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1, PICK_IMAGE_REQUEST = 2;
     ImageView imageView;
     TextView textView;
     Toolbar toolbar;
@@ -65,8 +72,12 @@ public class MainActivity extends AppCompatActivity {
         galleryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Go to gallery", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                }
+                int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+                if (result == 0)
+                    dispatchChoosePictureIntent();
             }
         });
     }
@@ -94,11 +105,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private void dispatchChoosePictureIntent(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        // Show only images, no videos or anything else
+        intent.setType("image/*");
+        // Always show the chooser (if there are multiple options available)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            //Bundle extras = data.getExtras();
             setPic();
+        }
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+            File file = new File(FileUtil.getPath(selectedImageUri, this));
+            String picturePath = file.getAbsolutePath();
+            System.out.println("picturepath " + picturePath);
+            currentPhotoPath = picturePath;
+            setPic();
+
         }
     }
 
@@ -107,14 +134,11 @@ public class MainActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
         // Save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
+        //System.out.println("image path: "+ currentPhotoPath);
         return image;
     }
 
@@ -131,13 +155,12 @@ public class MainActivity extends AppCompatActivity {
         int photoW = bmOptions.outWidth;
         int photoH = bmOptions.outHeight;
 
-        // Determine how much to scale down the image
+        // determine how much to scale down the image
         int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
 
-        // Decode the image file into a Bitmap sized to fill the View
+        // convert the image file into a Bitmap sized to fill the image_view
         bmOptions.inJustDecodeBounds = false;
         bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
 
         Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
 
@@ -145,20 +168,16 @@ public class MainActivity extends AppCompatActivity {
         float rotation = getPictureRotationFromCamera();
         bitmap = rotateBitmap(bitmap, rotation);
 
-        //create image to analyze and populate imageView
-        firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-
         //populate image_view with taken picture
         imageView.setImageBitmap(bitmap);
 
         //analyze picture and populate text_view with results
-        analyzePicture();
-
+        analyzePicture(bitmap);
     }
 
 
-    private void analyzePicture() {
-
+    private void analyzePicture(Bitmap bitmap) {
+        firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
         //create labeler
         FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
                 .getOnDeviceImageLabeler();
@@ -225,4 +244,5 @@ public class MainActivity extends AppCompatActivity {
             return 0;
         }
     }
+
 }
---
+++ app/src/main/java/com/bartz/whatsonpicture/PictureHandler.java
@@ -0,0 +1,14 @@
+package com.bartz.whatsonpicture;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PictureHandler {
+
+}
