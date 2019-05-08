4ca666f9658fb08063cec394eb267e98699bb4c0
BartZ <bartosz93zak@gmail.com>
2019-05-08 03:11:28 +0200

    final commit

--- app/src/main/java/com/bartz/whatsonpicture/MainActivity.java
+++ app/src/main/java/com/bartz/whatsonpicture/MainActivity.java
@@ -14,9 +14,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
@@ -27,18 +25,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
-import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1, PICK_IMAGE_REQUEST = 2;
@@ -46,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     Toolbar toolbar;
     String currentPhotoPath;
-    FirebaseVisionImage firebaseVisionImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dispatchTakePictureIntent();
-
             }
         });
 
@@ -116,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            setPic();
+            PictureHandler.setPic(imageView, currentPhotoPath, textView);
         }
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
             Uri selectedImageUri = data.getData();
@@ -124,7 +112,8 @@ public class MainActivity extends AppCompatActivity {
             String picturePath = file.getAbsolutePath();
             System.out.println("picturepath " + picturePath);
             currentPhotoPath = picturePath;
-            setPic();
+
+            PictureHandler.setPic(imageView, currentPhotoPath, textView);
 
         }
     }
@@ -140,109 +129,6 @@ public class MainActivity extends AppCompatActivity {
         currentPhotoPath = image.getAbsolutePath();
         //System.out.println("image path: "+ currentPhotoPath);
         return image;
-    }
-
-
-    private void setPic() {
-        // Get the dimensions of the View
-        int targetW = imageView.getWidth();
-        int targetH = imageView.getHeight();
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // determine how much to scale down the image
-        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
-
-        // convert the image file into a Bitmap sized to fill the image_view
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-
-        //get rotation and rotate bitmap
-        float rotation = getPictureRotationFromCamera();
-        bitmap = rotateBitmap(bitmap, rotation);
-
-        //populate image_view with taken picture
-        imageView.setImageBitmap(bitmap);
-
-        //analyze picture and populate text_view with results
-        analyzePicture(bitmap);
-    }
-
-
-    private void analyzePicture(Bitmap bitmap) {
-        firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-        //create labeler
-        FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
-                .getOnDeviceImageLabeler();
-
-        //pass image to labeler and decide on action to be taken
-        labeler.processImage(firebaseVisionImage)
-                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
-                    @Override
-                    public void onSuccess(List<FirebaseVisionImageLabel> labels) {
-                        StringBuilder result = new StringBuilder();
-
-                        for (FirebaseVisionImageLabel label : labels) {
-                            String text = label.getText();
-                            float confidence = label.getConfidence();
-                            result.append(text).append(" - ").append((int) Math.ceil(confidence * 100)).append("% | ");
-                        }
-                        textView.setText(result);
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.wtf("labaler error", e);
-                        textView.setText("no matches for your picture!");
-                    }
-                });
-    }
-
-    public static Bitmap rotateBitmap(Bitmap source, float angle) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(angle);
-        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
-                matrix, true);
-    }
-
-    private float getPictureRotationFromCamera() {
-        int rotation;
-        try {
-            ExifInterface ei = new ExifInterface(currentPhotoPath);
-            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
-                    ExifInterface.ORIENTATION_UNDEFINED);
-
-            switch (orientation) {
-
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    rotation = 90;
-                    break;
-
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    rotation = 180;
-                    break;
-
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    rotation = 270;
-                    break;
-
-                case ExifInterface.ORIENTATION_NORMAL:
-                default:
-                    rotation = 0;
-            }
-            return rotation;
-        } catch (IOException e) {
-            Log.wtf("getting orientation ", e);
-            return 0;
-        }
     }
 
 }
--- app/src/main/java/com/bartz/whatsonpicture/PictureHandler.java
+++ app/src/main/java/com/bartz/whatsonpicture/PictureHandler.java
@@ -1,14 +1,127 @@
 package com.bartz.whatsonpicture;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.io.File;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+
 import java.io.IOException;
+import java.util.List;
 
-public class PictureHandler {
+class PictureHandler {
+
+    static Bitmap rotateBitmap(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    static float getPictureRotationFromCamera(String currentPhotoPath) {
+        int rotation;
+        try {
+            ExifInterface ei = new ExifInterface(currentPhotoPath);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+
+            switch (orientation) {
+
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotation = 90;
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotation = 180;
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotation = 270;
+                    break;
+
+                case ExifInterface.ORIENTATION_NORMAL:
+                default:
+                    rotation = 0;
+            }
+            return rotation;
+        } catch (IOException e) {
+            Log.wtf("getting orientation ", e);
+            return 0;
+        }
+    }
+
+    static void setPic(ImageView imageView, String currentPhotoPath, TextView textView) {
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // convert the image file into a Bitmap sized to fill the image_view
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        //get rotation and rotate bitmap
+        float rotation = PictureHandler.getPictureRotationFromCamera(currentPhotoPath);
+        bitmap = PictureHandler.rotateBitmap(bitmap, rotation);
+
+        //populate image_view with taken picture
+        imageView.setImageBitmap(bitmap);
+
+        analyzePicture(bitmap, textView);
+
+    }
+
+    static void analyzePicture(Bitmap bitmap,  final TextView textView) {
+        FirebaseVisionImage firebaseVisionImage;
+        firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
+        //create labeler
+        FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                .getOnDeviceImageLabeler();
+
+        //pass image to labeler and decide on action to be taken
+        labeler.processImage(firebaseVisionImage)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                        StringBuilder result = new StringBuilder();
+
+                        for (FirebaseVisionImageLabel label : labels) {
+                            String text = label.getText();
+                            float confidence = label.getConfidence();
+                            result.append(text).append(" - ").append((int) Math.ceil(confidence * 100)).append("% | ");
+                        }
+                        textView.setText(result);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.wtf("labaler error", e);
+                        textView.setText("no matches for your picture!");
+                    }
+                });
+    }
+
 
 }
